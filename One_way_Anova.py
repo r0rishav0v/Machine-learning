032e0fb674b282055b820a309512c7fe77dedbc7
r0rishav0v <r0rishav0v@gmail.com>
2021-05-24 17:32:35 +0530

    Update One_way_Anova.py

--- One_way_Anova.py
+++ One_way_Anova.py
@@ -3,7 +3,6 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and setting
 import pandas as pd
-import numpy as np
 
 
 
